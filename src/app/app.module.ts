b95a85444972af141dde832d3711ce78c0cf81f6
DavidCuturilo <david.cuturilo@blinking.id>
2022-08-21 22:28:59 +0200

    feat: custom theme implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
+import { MaterialModule } from './material';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, MaterialModule, ReactiveFormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
